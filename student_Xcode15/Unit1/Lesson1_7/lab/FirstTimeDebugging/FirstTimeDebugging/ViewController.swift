4688a4c25417aede7d223b093b837e64137809f4
Charles Patrick Reinhardt <cpatrickreinhardt@gmail.com>
2024-11-06 22:50:39 -0500

    documentation

--- student_Xcode15/Unit1/Lesson1_7/lab/FirstTimeDebugging/FirstTimeDebugging/ViewController.swift
+++ student_Xcode15/Unit1/Lesson1_7/lab/FirstTimeDebugging/FirstTimeDebugging/ViewController.swift
@@ -2,6 +2,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    /// <#Description#>
     override func viewDidLoad(){
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
